bfe8c8210227fb4ee45c6413aff6a918ff3121b6
WltrH <WltrHrng@gmail.com>
2023-01-09 14:49:53 +0100

    Premier Commit fichier 2048

---
+++ 2048.py
@@ -0,0 +1,75 @@
+
+import numpy as np
+import random
+
+"""
+1-Commencez par définir la structure de votre grille de jeu. Vous pouvez utiliser 
+une liste à double entrée pour stocker les valeurs de chaque case de la grille.
+
+2-Ensuite, écrivez une fonction qui gère les déplacements de la grille lorsque 
+l'utilisateur utilise les flèches du clavier. Cette fonction devra prendre en compte les règles du jeu, 
+comme la fusion de cases de même valeur et l'interdiction de déplacer des cases vers des cases déjà occupées.
+
+3-Ajoutez une fonction qui gère l'ajout de nouvelles cases à la grille après chaque coup joué par l'utilisateur. 
+Cette fonction devra choisir au hasard une case vide et y insérer une nouvelle valeur (2 ou 4).
+
+4-Enfin, écrivez une fonction principale qui initialise la grille et gère les entrées de l'utilisateur jusqu'à ce que la partie
+ soit terminée (soit parce que la grille est pleine, soit parce que l'utilisateur a atteint la valeur cible 2048).
+
+Je vous recommande également d'utiliser la bibliothèque pygame pour gérer l'interface graphique de votre jeu. 
+Cela vous permettra de créer une grille affichée à l'écran et de gérer les entrées de l'utilisateur à l'aide de la bibliothèque pygame.
+
+"""
+
+
+#initialisation de la matrice por le jeux 2048
+
+#matrice extensible
+
+
+def init_grid (matrice):
+
+    #création variables pour permettre l'extensibilité de la matrice
+    maxi = len(matrice)     #lignes
+    mini = len(matrice[0])  #colonnes
+
+    #matrice = np.zeros((maxi,mini))
+    values = [0,2]
+    count = 0
+    for i in range (maxi):
+        for j in range  (mini):
+            #mise des valeurs à 0 et 2 avec un maximum de 2 valeurs à 2 et le reste à 0
+            if count < 2: 
+                matrice[i][j] = np.random.choice(values)
+                if matrice[i][j] == 2:
+                    count += 1
+                else:
+                     matrice[i][j] = 0
+
+
+#MMise en place de la fonction add_grid pour rajouter un élément dans la matrice (80% = 2 20% = 4)
+
+def add_digit(matrice):
+    #création variables pour permettre l'extensibilité de la matrice
+    maxi = len(matrice)     #lignes
+    mini = len(matrice[0])  #colonnes
+
+    count = 0
+
+    for i in range (maxi):
+        for j in range (mini):
+            rand = np.random.randint(10)
+
+            if rand < 7 and count < 1:
+                matrice[i][j] = 2
+            elif rand > 7 and count < 1:
+                matrice[i][j] = 4
+
+
+
+
+matrice = np.zeros((4,4))
+init_grid(matrice)
+print (matrice)
+add_digit(matrice)
+print (matrice)
